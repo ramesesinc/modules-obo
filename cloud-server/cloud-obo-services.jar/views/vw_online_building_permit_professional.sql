bfa81195fe4716cb6c1a713d039f06964dab48c8
jasondellecanonoy <jasondellecanonoy@yahoo.com.ph>
2019-07-11 19:35:35 +0800

    update

--- cloud-server/cloud-obo-services.jar/views/vw_online_building_permit_professional.sql
+++ cloud-server/cloud-obo-services.jar/views/vw_online_building_permit_professional.sql
@@ -2,6 +2,13 @@ DROP VIEW IF EXISTS vw_online_building_permit_professional;
 CREATE VIEW vw_online_building_permit_professional AS 
 SELECT 
    a.*,
-   e.name AS entity_name   
+   e.name AS entity_name,  
+   e.firstname as entity_firstname, 
+   e.lastname as entity_lastname, 
+   e.middlename as entity_middlename, 
+   e.address_text as entity_address_text, 
+   e.tin as entity_tin, 
+   e.id as entity_id 
 FROM online_building_permit_professional a 
 INNER JOIN online_building_permit_entity e ON a.entityid = e.objid
+;
