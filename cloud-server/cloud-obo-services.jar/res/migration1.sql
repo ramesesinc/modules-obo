d69589d5d8ad678a725cca1b7612448cf959c52a
elmonazareno <elmonazareno@gmail.com>
2019-11-27 13:39:07 +0800

    updated building_permit location

--- cloud-server/cloud-obo-services.jar/res/migration1.sql
+++ cloud-server/cloud-obo-services.jar/res/migration1.sql
@@ -1,3 +1,7 @@
+ALTER TABLE online_building_permit DROP COLUMN location_address1;
+ALTER TABLE online_building_permit DROP COLUMN location_address2;
 
-ALTER TABLE online_building_permit ADD COLUMN location_address1 varchar(255);
-ALTER TABLE online_building_permit ADD COLUMN location_address2 varchar(255);
+ALTER TABLE online_building_permit ADD COLUMN location_unitno varchar(25);
+ALTER TABLE online_building_permit ADD COLUMN location_bldgno varchar(25);
+ALTER TABLE online_building_permit ADD COLUMN location_bldgname varchar(25);
+ALTER TABLE online_building_permit ADD COLUMN location_subdivision varchar(25);
